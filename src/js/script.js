551c7bb2ed04e04e056a8a3ae5b4fa13918e720b
Julia Silva <jusilvadev@gmail.com>
2026-09-24 20:48:10 -0300

    adiciona botao de fechar ao video

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,21 @@ const video = document.getElementById("videoIntro");
 
 const btnEntrar = document.getElementById("btnEntrar");
 
+const btnFechar = document.getElementById("btnFechar");
+
+
 btnEntrar.addEventListener("click", () => {
 
     video.play();
 
     btnEntrar.style.display = "none";
 
+});
+
+btnFechar.addEventListener("click", () => {
+    video.pause();
+    video.currentTime = 0;
+    intro.style.display = "none";
 });
 
 video.addEventListener("ended", () => {
